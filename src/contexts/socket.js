c7daa3166537955197ff6bd1f887e8bbe1690387
Kevin Htun <khaingkhanthtun@gmail.com>
2022-04-13 18:38:37 +0630

    added conn failure handler

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -2,13 +2,19 @@ import socketClient from "socket.io-client";
 import { host } from "../environment";
 import { createContext } from "react";
 
+const connectErrHandler = async (err) => {
+    window.alert("Cannot connect to server, please login again!");
+    localStorage.clear();
+    window.location.reload();
+};
+
 export const initializeSocket = ({ token }) => {
     console.log("Init Socket Client");
     return socketClient(host, {
         auth: {
             token,
         },
-    });
+    }).once("connect_error", connectErrHandler);
 };
 export const SocketContext = createContext({
     socket: {},
